a37bb1b5b9d5d7411b192883608cb7a0e1c1c617
Adalberto Silveira  Napoles <asilveira@3htp.com>
2022-04-07 15:11:48 -0500

    Primer commit

---
+++ src/main/java/com/adalbertosn/springbootuploadfilewebclient/SpringBootUploadFileWebClientApplication.java
@@ -0,0 +1,14 @@
+package com.adalbertosn.springbootuploadfilewebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadFileWebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUploadFileWebClientApplication.class, args);
+        System.out.println("Ejecutando....");
+    }
+
+}
---
+++ src/test/java/com/adalbertosn/springbootuploadfilewebclient/SpringBootUploadFileWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adalbertosn.springbootuploadfilewebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUploadFileWebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
